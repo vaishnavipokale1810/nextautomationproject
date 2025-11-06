c01674a567d0187f7e18ab80080c880182c4bdb4
vaishnavipokale1810 <vaishnavipokale1810@gmail.com>
2025-11-06 23:24:26 +0530

    Initial Commit

---
+++ src/main/java/com/vaishnavi/nextautomationproject/App.java
@@ -0,0 +1,15 @@
+package com.vaishnavi.nextautomationproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Hello Selenium ");
+        System.out.println("Hello Selenium 2");
+    }
+}
